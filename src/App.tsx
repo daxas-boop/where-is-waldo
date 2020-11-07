ae63cc9815070dcb17f7803f32d044c6067b81da
daxas-boop <daxas.swd@hotmail.com>
2020-11-06 22:16:03 -0300

    Changes Header name to NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/header/Header';
+import Header from './components/navbar/NavBar';
 import Leaderboard from './components/leaderboard/Leaderboard';
 import Levels from './components/levels/Levels';
 import Home from './components/home/Home';
