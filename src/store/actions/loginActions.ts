42b073c7e5d72890e3bcb44516d18d5e725519fc
daxas-boop <daxas.swd@hotmail.com>
2020-11-10 19:31:30 -0300

    Adds template for loginActions

--- src/store/actions/loginActions.ts
+++ src/store/actions/loginActions.ts
@@ -1,3 +1,23 @@
-export const userLogin = () => {
-  return { type: 'LOGIN_SUCESS' };
+import firebase from '../../config/fbConfig';
+
+export const logIn = (email: string, password: string) => {
+  return async (dispatch: any, getState: any) => {
+    try {
+      firebase.auth().signInWithEmailAndPassword(email, password);
+      dispatch({ type: 'USER_LOGGED_IN' });
+    } catch (error) {
+      dispatch({ type: 'USER_LOGGED_IN_ERROR' });
+    }
+  };
+};
+
+export const signUp = (email: string, password: string) => {
+  return async (dispatch: any, getState: any) => {
+    try {
+      firebase.auth().createUserWithEmailAndPassword(email, password);
+      dispatch({ type: 'SIGNUP_SUCCESS' });
+    } catch (error) {
+      dispatch({ type: 'SIGNUP_ERROR' });
+    }
+  };
 };
