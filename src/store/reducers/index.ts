665486cebc064feef930a595dd218ae9cf404f1c
daxas-boop <daxas.swd@hotmail.com>
2020-11-10 19:31:18 -0300

    Adds template for rootReducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,7 +1,23 @@
 const initState = {};
 
-const myReducer = (state = initState, action: {}) => {
+const rootReducer = (state = initState, action: { type: string }) => {
+  switch (action.type) {
+    case 'LOGIN_SUCCESS':
+      console.log('login success');
+      break;
+    case 'LOGIN_ERROR':
+      console.log('login error');
+      break;
+    case 'SIGNUP_SUCCESS':
+      console.log('signup success');
+      break;
+    case 'SIGNUP_ERROR':
+      console.log('signup error');
+      break;
+    default:
+      return state;
+  }
   return state;
 };
 
-export default myReducer;
+export default rootReducer;
