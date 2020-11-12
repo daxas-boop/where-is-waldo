483feda388444b512fa679f17315b53bcb7e268d
daxas-boop <daxas.swd@hotmail.com>
2020-11-11 23:29:23 -0300

    Adds logout action

--- src/components/navbar/SignedInLinks.tsx
+++ src/components/navbar/SignedInLinks.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout as logoutAction } from '../../store/actions/loginActions';
 
 const SignedInLinks = () => {
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(logoutAction());
+  };
+
+  const state = useSelector((state) => state);
+
   return (
     <>
       <Avatar>H</Avatar>
-      <Button>Logout</Button>
+      <Button onClick={logout}>Logout</Button>
     </>
   );
 };
